4c15f329127ba9eeec99075a2f23fb92343ec0ed
veskakoff <veskakov@gmail.com>
2012-03-08 16:25:29 +0300

    Created Posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enblog::Application.config.secret_token = '74f8558f1612471991584060ccc1eaa8872724fbb26a4949df5c5065abffd5cf9b538a3509b8cbb91345e7b322f456924dd3e1fdf3c417be906ca282d906b148'
